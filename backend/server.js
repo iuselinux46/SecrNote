1bc8eaaeaa4ee7c427693039fc79ad2013a50f24
iuselinux46 <architmalgundkar@gmail.com>
2026-08-01 00:34:21 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({
   origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
   methods: ["GET", "POST"],
+  process.env.FRONTEND_URL,
 }));
 
 app.use(express.json());
