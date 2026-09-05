3c0c36feadabefc4e9d291f0a4a509a2562a6fb9
iuselinux46 <architmalgundkar@gmail.com>
2026-09-05 21:54:36 +0530

    Major Email Changes

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,8 +1,5 @@
 require("dotenv").config();
 
-console.log("BREVO_API_KEY length:", process.env.BREVO_API_KEY?.length);
-console.log("BREVO_API_KEY starts with:", process.env.BREVO_API_KEY?.substring(0, 12));
-
 async function sendReadReceipt(to, timestamp) {
   console.log("Sending read receipt to:", to);
   try {
